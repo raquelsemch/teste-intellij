2a089c14bcb41c72667d7f26f3c744522a194c63
raquelsemch <raquelsemch@gmail.com>
2022-07-22 22:59:23 -0300

    criando class livro

--- src/br/com/dio/primeiroprograma.java
+++ src/br/com/dio/primeiroprograma.java
@@ -8,9 +8,48 @@ public class primeiroprograma {
 
         System.out.println(gato);
 
+        livro Livro1 = new livro("Sagarana", 450);
+
+        System.out.println(Livro1);
+
         /*int a = 2;
         int b = 3;
 
         System.out.println("Hello World! " + (a+b));*/
+    }
+}
+
+class livro {
+    private String nome;
+    private Integer numPaginas;
+
+    public livro(String nome, Integer numPaginas) {
+        this.nome = nome;
+        this.numPaginas = numPaginas;
+
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getNumPaginas() {
+        return numPaginas;
+    }
+
+    public void setNumPaginas(Integer numPaginas) {
+        this.numPaginas = numPaginas;
+    }
+
+    @Override
+    public String toString() {
+        return "livro{" +
+                "nome='" + nome + '\'' +
+                ", numPaginas=" + numPaginas +
+                '}';
     }
 }
